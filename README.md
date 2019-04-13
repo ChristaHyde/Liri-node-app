342157516bbe200bd7d6c0021b49bfc7af5b9b5f
Christa <christahyde@yahoo.com>
2019-04-13 13:50:31 -0600

    I added Spotify and Axios

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Liri-node-app
 Homework Assignment #8
+
+1. When the user types in the command line -- node liri.js concert-this and an artist
+This information will be populated:
+     * Name of the venue
+
+     * Venue location
+
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+2. When the user types in the command line -- node liri.js spotify-this-song
+This information will be populated:
+   * Artist(s)
+
+     * The song's name
+
+     * A preview link of the song from Spotify
+
+     * The album that the song is from
+
+3. When the user types in the command line -- node liri.js movie-this
+This information will be populated:
+  * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+
+4. When the user types in the command line -- node liri.js do-what-it-says
+This information will be populated:
+ * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
---
+++ liri.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+
+var axios = require("axios");
+
+var Spotify = require('node-spotify-api');
+
+var Concert = require("codingbootcamp");
+
+var keys = require("./keys.js");
+
+var command = process.argv[2];
+var query = process.argv.slice(3).join(" ");
+console.log(command);
+console.log(query);
+
+if (command === "spotify-this") {
+    console.log("running-spotify");
+    var spotify = new Spotify(keys.spotify);
+    // console.log(keys);
+
+    spotify.search({ type: 'track', query: query }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data.tracks.items[0].album.artists);
+    });
+} else if (command === "concert-this") {
+    console.log("running-concert");
+    queryURL("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then()
+
+} else if (command === "movie-this") {
+    axios.get('http://www.omdbapi.com/?apikey=trilogy&t=' + query)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    console.log("running-movie");
+}
+
+
+
+
+
+console.log(process.argv);
+
+
+
+
+// `concert-this`
+
+//     `spotify-this-song`
+
+//     `movie-this`
+
+//     `do-what-it-says`
+
